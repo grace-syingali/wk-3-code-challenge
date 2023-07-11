a939178906b1766c3d9d917f608438704bac11ae
grace <grace.syingali@student.moringaschool.com>
2023-07-11 08:32:04 +0300

    completed assignment

--- app.js
+++ app.js
@@ -1,81 +1,75 @@
-// Fetch the first movie's details when the page loads
-fetch('db.json')
-  .then(response => response.json())
-  .then(data => {
-    // Update the DOM with the movie details
-    const movieDetails = document.querySelector('.movie-details');
-    const poster = movieDetails.querySelector('.poster');
-    const title = movieDetails.querySelector('.title');
-    const runtime = movieDetails.querySelector('.runtime');
-    const showtime = movieDetails.querySelector('.showtime');
-    const tickets = movieDetails.querySelector('.tickets');
-    const buyTicketBtn = movieDetails.querySelector('.buy-ticket');
+function fetchFilms() {
+  fetch('https://api.npoint.io/0b2aef194151f5771a43/films/')
+    .then(res => res.json())
+    .then(data => renderFilms(data));
+}
 
-    poster.src = data.poster;
-    title.textContent = data.title;
-    runtime.textContent = `Runtime: ${data.runtime} minutes`;
-    showtime.textContent = `Showtime: ${data.showtime}`;
-    tickets.textContent = `Tickets: ${data.capacity - data.tickets_sold} available`;
 
-    // Buy Ticket button event listener
-    buyTicketBtn.addEventListener('click', () => {
-      const availableTickets = data.capacity - data.tickets_sold;
-      if (availableTickets > 0) {
-        data.tickets_sold++;
-        tickets.textContent = `Tickets: ${data.capacity - data.tickets_sold} available`;
+
+function renderFilms(data) {
+  const div = document.getElementById('card');
+  const ul = document.getElementById('films');
+  
+
+
+  data.forEach(movie => {
+    const li = document.createElement('li');
+    li.classList.add('pointer', 'bold-italic-text');
+    li.innerHTML = movie.title;
+
+
+    const filmCard = document.createElement("div");
+    filmCard.classList.add('film-card');
+    filmCard.innerHTML = `
+      <img src="${movie.poster}" height=500px width=300px/>
+      <h2 class="bold-text">${movie.title}</h2>
+      <p class="bold-text">${movie.description}</p>
+      <p><span class="highlight bold-text">Runtime: ${movie.runtime}</span></p>
+      <p><span class="highlight bold-text">Showtime: ${movie.showtime}</span></p>
+    `;
+    // Create a new <p> element to display the number of available tickets
+    const tickets = document.createElement("p");
+    tickets.classList.add("bold-italic-text")
+    tickets.innerHTML = `Available tickets: ${(movie.capacity) - (movie.tickets_sold)}`;
+    // Append the new <p> element to the filmCard
+    filmCard.appendChild(tickets);
+
+
+
+
+
+    const btn = document.createElement("button");
+    btn.textContent = "Buy ticket";
+    // Add an event listener to the button to decrement the number of tickets when clicked
+    btn.addEventListener('click', () => {
+      // Check if the number of available tickets is 0
+      if (parseInt(tickets.innerText.split(': ')[1]) === 0) {
+        // If it is, show an alert
+        alert("Ticket Sold Out");
+      } else {
+        // Otherwise, decrement the number of available tickets by 1
+        tickets.innerText = `Available tickets: ${parseInt(tickets.innerText.split(': ')[1]) - 1}`;
       }
     });
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+    // Append the button to the filmCard
+    filmCard.appendChild(btn);
 
-// Fetch all movies for the menu
-fetch('/films')
-  .then(response => response.json())
-  .then(data => {
-    // Populate the films menu
-    const filmsList = document.getElementById('films');
-    filmsList.innerHTML = '';
 
-    data.forEach(film => {
-      const li = document.createElement('li');
-      li.classList.add('film', 'item');
-      li.textContent = film.title;
-      filmsList.appendChild(li);
-    });
 
-    // Film menu item click event listener
-    filmsList.addEventListener('click', (event) => {
-      if (event.target.classList.contains('film')) {
-        const filmId = event.target.dataset.filmId;
-        fetch(`/films/${filmId}`)
-          .then(response => response.json())
-          .then(filmData => {
-            // Update the movie details with the selected film's data
-            const movieDetails = document.querySelector('.movie-details');
-            const poster = movieDetails.querySelector('.poster');
-            const title = movieDetails.querySelector('.title');
-            const runtime = movieDetails.querySelector('.runtime');
-            const showtime = movieDetails.querySelector('.showtime');
-            const tickets = movieDetails.querySelector('.tickets');
-            const buyTicketBtn = movieDetails.querySelector('.buy-ticket');
+    li.addEventListener('click', () => {
+      div.innerText=""
+      div.appendChild(filmCard);
+       // Check if the filmCard has the "active" class
+       if (!filmCard.classList.contains('active')) {
+        // If it does not, add the "active" class and append the filmCard to the li element
+        filmCard.classList.add('active');  
+        div.appendChild(filmCard);
 
-            poster.src = filmData.poster;
-            title.textContent = filmData.title;
-            runtime.textContent = `Runtime: ${filmData.runtime} minutes`;
-            showtime.textContent = `Showtime: ${filmData.showtime}`;
-            tickets.textContent = `Tickets: ${filmData.capacity - filmData.tickets_sold} available`;
-
-            // Enable the Buy Ticket button
-            buyTicketBtn.disabled = false;
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
       }
+      
+    
     });
-  })
-  .catch(error => {
-    console.error('Error:', error);
+    ul.appendChild(li);
   });
+}
+fetchFilms();
